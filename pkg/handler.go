9c83c2c392b2b55338e37c77356d12507edf954d
Kasumu Sofiyu <kasumusof@gmail.com>
2022-06-13 11:04:02 +0100

    refactor code

--- pkg/handler.go
+++ pkg/handler.go
@@ -33,109 +33,15 @@ func Router() *chi.Mux {
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
 
-	r.Post("/items", func(w http.ResponseWriter, r *http.Request) {
-		var item Item
-		if err := render.Bind(r, &item); err != nil {
-			log.Println(err)
-			renderResponse(w, map[string]string{"message": "failed to bind"}, http.StatusBadRequest)
-			return
-		}
-		v, err := createItem(r.Context(), &item)
-		if err != nil {
-			log.Println(err)
-			renderResponse(w, map[string]string{"message": "failed to create item"}, http.StatusBadRequest)
-			return
-		}
+	r.Post("/items", create)
 
-		renderResponse(w, Respsone{
-			Message: "Ok",
-			Data:    v,
-		}, http.StatusCreated)
-	})
+	r.Get("/items", get)
 
-	r.Get("/items", func(w http.ResponseWriter, r *http.Request) {
-		status := r.URL.Query().Get("status")
-		v, err := listItems(r.Context(), status)
-		if err != nil {
-			log.Println(err)
-			renderResponse(w, map[string]string{"message": "failed to get items"}, http.StatusInternalServerError)
-			return
+	r.Delete("/items/{id}", delete)
 
-		}
-		renderResponse(w, Respsone{
-			Message: "Ok",
-			Data:    v,
-		}, http.StatusOK)
-	})
-
-	r.Get("/items/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := chi.URLParam(r, "id")
-		v, err := getItem(r.Context(), id)
-		if err != nil {
-			log.Println(err)
-			renderResponse(w, map[string]string{"message": "failed to get item"}, http.StatusInternalServerError)
-			return
-		}
-		renderResponse(w, Respsone{
-			Message: "Ok",
-			Data:    v,
-		}, http.StatusOK)
-	})
-
-	r.Delete("/items/{id}", func(w http.ResponseWriter, r *http.Request) {
-		var comment DeletedComment
-		if err := render.Bind(r, &comment); err != nil {
-			log.Println(err)
-			renderResponse(w, map[string]string{"message": "failed to bind"}, http.StatusBadRequest)
-			return
-		}
-
-		id := chi.URLParam(r, "id")
-		err := deleteItem(r.Context(), id, &comment)
-		if err != nil {
-			log.Println(err)
-			renderResponse(w, map[string]string{"message": "failed to delete item"}, http.StatusInternalServerError)
-			return
-		}
-		renderResponse(w, Respsone{
-			Message: "Ok",
-			Data:    id,
-		}, http.StatusOK)
-	})
-
-	r.Patch("/items/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := chi.URLParam(r, "id")
-		var item Item
-		if err := render.Bind(r, &item); err != nil {
-			log.Println(err)
-			renderResponse(w, map[string]string{"message": "failed to bind"}, http.StatusBadRequest)
-			return
-		}
-		err := updateItem(r.Context(), id, &item)
-		if err != nil {
-			log.Println(err)
-			renderResponse(w, map[string]string{"message": "failed to update item"}, http.StatusInternalServerError)
-			return
-		}
-		renderResponse(w, Respsone{
-			Message: "Ok",
-			Data:    "Item updated successfully",
-		}, http.StatusOK)
-	})
+	r.Patch("/items/{id}", update)
 	
-	r.Put("/items/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := chi.URLParam(r, "id")
-		err := unArchiveItem(r.Context(), id)
-		if err != nil {
-			log.Println(err)
-			renderResponse(w, map[string]string{"message": "failed to restore item"}, http.StatusInternalServerError)
-			return
-		}
-		renderResponse(w, Respsone{
-			Message: "Ok",
-			Data:    "Item restored successfully",
-		}, http.StatusOK)
-	})
+	r.Put("/items/{id}", restore)
 
 	FileServer(r)
 	return r
@@ -153,4 +59,99 @@ func FileServer(router *chi.Mux) {
 			fs.ServeHTTP(w, r)
 		}
 	})
+}
+
+// create item handler
+func create(w http.ResponseWriter, r *http.Request) {
+	var item Item
+	if err := render.Bind(r, &item); err != nil {
+		log.Println(err)
+		renderResponse(w, map[string]string{"message": "failed to bind"}, http.StatusBadRequest)
+		return
+	}
+	v, err := createItem(r.Context(), &item)
+	if err != nil {
+		log.Println(err)
+		renderResponse(w, map[string]string{"message": "failed to create item"}, http.StatusBadRequest)
+		return
+	}
+
+	renderResponse(w, Respsone{
+		Message: "Ok",
+		Data:    v,
+	}, http.StatusCreated)
+}
+
+// get items handler
+func get(w http.ResponseWriter, r *http.Request) {
+	status := r.URL.Query().Get("status")
+	v, err := listItems(r.Context(), status)
+	if err != nil {
+		log.Println(err)
+		renderResponse(w, map[string]string{"message": "failed to get items"}, http.StatusInternalServerError)
+		return
+
+	}
+	renderResponse(w, Respsone{
+		Message: "Ok",
+		Data:    v,
+	}, http.StatusOK)
+}
+
+// delete item handler
+func delete(w http.ResponseWriter, r *http.Request) {
+	var comment DeletedComment
+	if err := render.Bind(r, &comment); err != nil {
+		log.Println(err)
+		renderResponse(w, map[string]string{"message": "failed to bind"}, http.StatusBadRequest)
+		return
+	}
+
+	id := chi.URLParam(r, "id")
+	err := deleteItem(r.Context(), id, &comment)
+	if err != nil {
+		log.Println(err)
+		renderResponse(w, map[string]string{"message": "failed to delete item"}, http.StatusInternalServerError)
+		return
+	}
+	renderResponse(w, Respsone{
+		Message: "Ok",
+		Data:    id,
+	}, http.StatusOK)
+}
+
+// update item handler
+func update(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	var item Item
+	if err := render.Bind(r, &item); err != nil {
+		log.Println(err)
+		renderResponse(w, map[string]string{"message": "failed to bind"}, http.StatusBadRequest)
+		return
+	}
+	err := updateItem(r.Context(), id, &item)
+	if err != nil {
+		log.Println(err)
+		renderResponse(w, map[string]string{"message": "failed to update item"}, http.StatusInternalServerError)
+		return
+	}
+	renderResponse(w, Respsone{
+		Message: "Ok",
+		Data:    id,
+	}, http.StatusOK)
+}
+
+// restore item handler
+func restore(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	err := unArchiveItem(r.Context(), id)
+	if err != nil {
+		log.Println(err)
+		renderResponse(w, map[string]string{"message": "failed to restore item"}, http.StatusInternalServerError)
+		return
+	}
+	renderResponse(w, Respsone{
+		Message: "Ok",
+		Data:   id,
+	}, http.StatusOK)
 }
